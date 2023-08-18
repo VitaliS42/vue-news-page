f2af849dc2c09c9b7b4625bc8b99be2db1297676
Vitaly Savinov <vitalanother@gmail.com>
2023-08-18 08:54:47 +0300

    Logic added

---
+++ src/components/Banner.vue
@@ -0,0 +1,10 @@
+<template>
+  <section class="banner">
+    <h2>Новости</h2>
+  </section>
+</template>
+
+<style scoped>
+  .banner {padding:227px 100px 48px;background:url(/public/img/header-banner.svg);background-repeat:no-repeat;background-position:bottom center;height:320px;background-size:cover}
+  h2 {font-size:40px;font-weight:700;line-height:48px;letter-spacing:-0.01em;text-align:left}
+</style>
